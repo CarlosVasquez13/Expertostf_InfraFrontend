9518c78b1f819925a5d989cd2d67cc10793f03ba
CarlosVasquez13 <carlos1305vasquez@gmai.com>
2025-04-03 01:23:40 -0600

    Add .gitignore and Terraform variable definitions

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "subscription_id" {
+  description = "The ID of the Azure subscription where the resources will be created."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group where the resources will be created."
+  type        = string
+  default = "expertostf"
+}
+
+variable "environment" {
+  description = "The environment for the resources (e.g., dev, test, prod)."
+  type        = string
+  default     = "dev"
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be created."
+  type        = string
+  default     = "Central US"
+}
+
+variable "tags" {
+  description = "Tags to be applied to the resources."
+  type        = map(string)
+  default     = {
+    Environment = "dev"
+    date = "abril-2025"
+    Owner       = "CarlosAmaya"
+  }
+}
